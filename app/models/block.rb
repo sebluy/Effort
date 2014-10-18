8927d9821fd37be07122edccb5c5f9adefaad05e
Sebastian Luy <sebastian.a.luy@gmail.com>
2014-10-17 21:10:45 -0400

    Move some time calculation logic into models

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,5 +1,14 @@
 class Block < ActiveRecord::Base
 
   belongs_to :discipline
+
+  def time_spent
+    if finish.nil?
+      Time.zone.now - start
+    else
+      finish - start
+    end
+  end
+      
   
 end
--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,5 +1,9 @@
 class Discipline < ActiveRecord::Base
 
   has_many :blocks
+
+  def total_time_spent
+    blocks.inject(0) { |sum, block| sum + block.time_spent }
+  end
   
 end
