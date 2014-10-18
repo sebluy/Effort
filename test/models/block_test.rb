21d9a07d35104f7be51f4124689e62d57ef1cb85
Sebastian Luy <sebastian.a.luy@gmail.com>
2014-10-17 22:18:44 -0400

    Add unit tests for models

--- test/models/block_test.rb
+++ test/models/block_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class BlockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should calculate time spent correctly' do
+    assert_equal 60, blocks(:one).time_spent
+  end
+
 end
--- test/models/discipline_test.rb
+++ test/models/discipline_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class DisciplineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'should calculate total time spent correctly' do
+    assert_equal 120, disciplines(:coding).total_time_spent
+  end
+
 end
