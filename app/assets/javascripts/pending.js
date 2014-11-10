b77da0f95c1ca5cf7c788f2eb4f7fa2faec7bfa8
Sebastian Luy <sebastian.a.luy@gmail.com>
2014-11-09 22:44:46 -0500

    Include timer in title

--- app/assets/javascripts/pending.js
+++ app/assets/javascripts/pending.js
@@ -1,14 +1,20 @@
+/* this code is going to need some major refactoring after I learn
+   the javascripts */
 var timer ;
 var ready = function() {
   var date = new Date(0) ;
   clearInterval(timer) ;
-  $('#pending span#time').html(date.toUTCString().substring(17, 25)) ;
-  timer = setInterval(function() {myTimer(date)}, 1000) ;
+  if ($('#pending span#time').html() != undefined) {
+    $('#pending span#time').html(date.toUTCString().substring(17, 25)) ;
+    $('head title').html(date.toUTCString().substring(17, 25)) ;
+    timer = setInterval(function() {myTimer(date)}, 1000) ;
+  }
 } ;
 
 var myTimer = function(date) {
   date.setSeconds(date.getSeconds() + 1) ;
   $('#pending span#time').html(date.toUTCString().substring(17, 25)) ;
+  $('head title').html(date.toUTCString().substring(17, 25)) ;
 } ;
 
 $(document).ready(ready) ;
