400c53935493525cd7de284720f31b73549acbc7
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-03-22 15:35:44 -0400

    Start cleaning up discipline views

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -1,2 +1,9 @@
 module BlocksHelper
+
+  def finish_button(block)
+    link_to 'Finish',
+        finish_discipline_block_path(block.discipline, block),
+        method: :post, class: 'btn btn-sm btn-danger'
+  end
+
 end
--- app/helpers/disciplines_helper.rb
+++ app/helpers/disciplines_helper.rb
@@ -1,2 +1,8 @@
 module DisciplinesHelper
+
+  def start_button(discipline)
+    link_to 'Start', start_discipline_block_path(discipline),
+      method: :post, class: 'btn btn-sm btn-success'
+  end
+
 end
